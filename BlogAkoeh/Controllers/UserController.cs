4a15b0b3a9716ba7719f542087f434f2011d8b5c
Arie <Rozindzakwan@gmail.com>
2023-03-18 22:09:39 +0700

    Upload File & Validation

--- BlogAkoeh/Controllers/UserController.cs
+++ BlogAkoeh/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using BlogAkoeh.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Security.Claims;
 
 namespace BlogAkoeh.Controllers
@@ -10,10 +11,12 @@ namespace BlogAkoeh.Controllers
     public class UserController : Controller
     {
         private readonly MysqlContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public UserController(MysqlContext c)
+        public UserController(MysqlContext c, IWebHostEnvironment env)
         {
             _context = c;
+            _env = env;
         }
 
         public IActionResult Index()
@@ -23,14 +26,49 @@ namespace BlogAkoeh.Controllers
             return View(users);
         }
 
+        public IActionResult Detail(int id)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == id);
+            
+            return View(user);
+        }
+
+        public IActionResult Download(int id)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == id);
+            var filepath = Path.Combine(_env.WebRootPath, "upload", user.Photo);
+
+            return File(System.IO.File.ReadAllBytes(filepath), "image/png", Path.GetFileName(filepath));
+        }
+
         public IActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Create([FromForm] User user)
+        public IActionResult Create([FromForm] User user, IFormFile Photo)
         {
+            if(Photo.Length > 100000)
+            {
+                ModelState.AddModelError(nameof(user.Photo), "Photo size is too big");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var filename = "photo_" + user.Username + Path.GetExtension(Photo.FileName);
+            var filepath = Path.Combine(_env.WebRootPath, "upload", filename);
+
+            using (var stream = System.IO.File.Create(filepath))
+            {
+                Photo.CopyTo(stream);
+            }
+
+            user.Photo = filename;
+
             _context.Users.Add(user);
             _context.SaveChanges();
 
---
+++ BlogAkoeh/Migrations/20230318132721_photo_user.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogAkoeh.Migrations
+{
+    /// <inheritdoc />
+    public partial class photouser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "Users",
+                type: "longtext",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Users");
+        }
+    }
+}
